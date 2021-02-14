e92ead72883fd30afe5ad68b1796147c167b8e3d
IDLKZ <idlkz2019@gmail.com>
2021-02-14 12:27:10 +0600

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-system-bar
+      window
+      dark
+      class="px-0"
+    >
+      <v-tabs>
+        <v-tab>О нас</v-tab>
+        <v-tab>Реклама</v-tab>
+        <v-tab>Карьера</v-tab>
+        <v-tab>Контакты</v-tab>
+      </v-tabs>
+
+      <v-spacer></v-spacer>
+      <v-icon>mdi-minus</v-icon>
+      <v-icon>mdi-checkbox-blank-outline</v-icon>
+      <v-icon>mdi-close</v-icon>
+    </v-system-bar>
+
+
+
+
+  </v-app>
+</template>
+<script>
+export default {
+
+}
+
+</script>
+<style>
+
+</style>
